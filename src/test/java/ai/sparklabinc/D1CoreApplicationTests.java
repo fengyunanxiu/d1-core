08d0890f70b96382743214f4a43a5188389d89a6
DAM <1414028747@qq.com>
2019-07-01 16:00:49 +0800

    commit

--- src/test/java/ai/sparklabinc/D1CoreApplicationTests.java
+++ src/test/java/ai/sparklabinc/D1CoreApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FdvtCoreApplicationTests {
+public class D1CoreApplicationTests {
 
     @Test
     public void contextLoads() {
