a8506becbbea0aa21623bded38ddb278032264d6
DAM <1414028747@qq.com>
2019-07-02 10:34:42 +0800

    commit

---
+++ src/main/java/ai/sparklabinc/service/DataSourceService.java
@@ -0,0 +1,12 @@
+package ai.sparklabinc.service;
+
+/**
+ * @Function:
+ * @Author: DAM
+ * @Date: 2019/7/1 20:32
+ * @Description:
+ * @Version V1.0
+ */
+public interface DataSourceService {
+    Object getDataSourcesTree();
+}
---
+++ src/main/java/ai/sparklabinc/service/impl/DataSourceServiceImpl.java
@@ -0,0 +1,26 @@
+//package ai.sparklabinc.service.impl;
+//
+//import ai.sparklabinc.service.DataSourceService;
+//import com.baomidou.dynamic.datasource.annotation.DS;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * @Function:
+// * @Author: DAM
+// * @Date: 2019/7/1 20:33
+// * @Description:
+// * @Version V1.0
+// */
+//@DS("QA")
+//@Service
+//public class DataSourceServiceImpl implements DataSourceService {
+//    @Autowired
+//    private  JdbcTemplate jdbcTemplate;
+//    @Override
+//    public Object getDataSourcesTree() {
+//        String sql="";
+//        return null;
+//    }
+//}
