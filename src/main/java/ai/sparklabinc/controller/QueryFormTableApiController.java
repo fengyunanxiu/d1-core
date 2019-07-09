8bd69a1c5ea1e4d551ff60e67abc989e22c0b959
尹凯 <756792422@qq.com>
2019-07-09 16:33:30 +0800

    [*]  fix

--- src/main/java/ai/sparklabinc/controller/QueryFormTableApiController.java
+++ src/main/java/ai/sparklabinc/controller/QueryFormTableApiController.java
@@ -5,6 +5,7 @@ import ai.sparklabinc.exception.custom.IllegalParameterException;
 import ai.sparklabinc.exception.custom.ResourceNotFoundException;
 import ai.sparklabinc.service.QueryFormTableService;
 import ai.sparklabinc.util.ApiUtils;
+import ai.sparklabinc.util.ParameterHandlerUtils;
 import com.mysql.jdbc.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -93,8 +94,8 @@ public class QueryFormTableApiController {
         if (StringUtils.isNullOrEmpty(dataSourceKey)) {
             throw new ResourceNotFoundException("Empty data source key " + dataSourceKey);
         }
-        Pageable pageable = extractPageable(params);
-        String moreWhereClause = extractMoreClause(params);
+        Pageable pageable = ParameterHandlerUtils.extractPageable(params);
+        String moreWhereClause = ParameterHandlerUtils.extractMoreClause(params);
         Map<String, String[]> simpleParameters = ApiUtils.removeReservedParameters(params);
         return queryFormTableService.generalQuery(dataSourceKey, simpleParameters, pageable,moreWhereClause,false);
     }
@@ -113,8 +114,8 @@ public class QueryFormTableApiController {
         if (StringUtils.isNullOrEmpty(dataSourceKey)) {
             throw new ResourceNotFoundException("Empty data source key " + dataSourceKey);
         }
-        Pageable pageable = extractPageable(params);
-        String moreWhereClause = extractMoreClause(params);
+        Pageable pageable = ParameterHandlerUtils.extractPageable(params);
+        String moreWhereClause = ParameterHandlerUtils.extractMoreClause(params);
         Map<String, String[]> simpleParameters = ApiUtils.removeReservedParameters(params);
         return queryFormTableService.generalQuery(dataSourceKey, simpleParameters, pageable,moreWhereClause,true);
     }
@@ -127,7 +128,7 @@ public class QueryFormTableApiController {
      * @param dataSourceKey
      * @param request
      * @return
-     * @throws Exception
+     * @throws Exceptions
      */
     @GetMapping("/execute-query")
     @ResponseBody
@@ -138,63 +139,14 @@ public class QueryFormTableApiController {
         }
         Map<String, String[]> params = request.getParameterMap();
 
-        Pageable pageable = extractPageable(params);
-        String moreWhereClause = extractMoreClause(params);
+        Pageable pageable = ParameterHandlerUtils.extractPageable(params);
+        String moreWhereClause = ParameterHandlerUtils.extractMoreClause(params);
         Map<String, String[]> simpleParameters = ApiUtils.removeReservedParameters(params);
         return  queryFormTableService.executeQuery(dataSourceKey, simpleParameters, pageable,moreWhereClause);
     }
 
 
 
-    public static Pageable extractPageable(Map<String, String[]> params) {
-        String[] pages = params.get(QueryParamConstants.SQL_PARAMS_KEY_FOR_SQL_PAGE);
-        String[] sizes = params.get(QueryParamConstants.SQL_PARAMS_KEY_FOR_SQL_SIZE);
-        String[] sorts = params.get(QueryParamConstants.SQL_PARAMS_KEY_FOR_SQL_SORT);
-        Integer page = null;
-        Integer size = null;
-        Sort sort = null;
-        if (pages != null && pages.length > 0) {
-            page = Integer.valueOf(pages[0]);
-        }
-        if (sizes != null && sizes.length > 0) {
-            size = Integer.valueOf(sizes[0]);
-        }
-        if (sorts != null && sorts.length > 0) {
-            for (int i = 0; i < sorts.length; i++) {
-                String sortParam = sorts[i];
-                String[] sortArray = sortParam.split(",");
-                Sort sigleSort = null;
-                if (sortArray.length == 1) {
-                    sigleSort = new Sort(Sort.Direction.DESC, sorts[0]);
-                } else if (sortArray.length == 2) {
-                    String direction = sortArray[1];
-                    String fieldName = sortArray[0];
-                    if (QueryParamConstants.SQL_PARAMS_KEY_FOR_SQL_SORT_DESC.equalsIgnoreCase(direction)) {
-                        sigleSort = new Sort(Sort.Direction.DESC, fieldName);
-                    } else if (QueryParamConstants.SQL_PARAMS_KEY_FOR_SQL_SORT_ASC.equalsIgnoreCase(direction)) {
-                        sigleSort = new Sort(Sort.Direction.ASC, fieldName);
-                    }
-                }
-                if(sort == null) {
-                    sort = sigleSort;
-                }else {
-                    sort = sort.and(sigleSort);
-                }
-            }
-        }
-        if (page != null && size != null) {
-            return  PageRequest.of(page, size, sort);
-        }
-        return null;
-    }
 
-    public static String extractMoreClause(Map<String, String[]> params) {
-        String moreWhereClause = null;
-        String[] moreWhereClauses = params.get(QueryParamConstants.SQL_PARAMS_KEY_FOR_CUSTOMER_SQL_CONDITION_CLAUSE);
-        if (moreWhereClauses != null && moreWhereClauses.length > 0) {
-            moreWhereClause = " and " + moreWhereClauses[0];
-        }
-        return moreWhereClause;
-    }
 
 }
---
+++ src/main/java/ai/sparklabinc/util/ParameterHandlerUtils.java
@@ -0,0 +1,71 @@
+package ai.sparklabinc.util;
+
+import ai.sparklabinc.constant.QueryParamConstants;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.Map;
+
+/**
+ * @author : Kingzer
+ * @date : 2019-07-09 16:29
+ * @description :
+ */
+public class ParameterHandlerUtils {
+
+
+    public static Pageable extractPageable(Map<String, String[]> params) {
+        String[] pages = params.get(QueryParamConstants.SQL_PARAMS_KEY_FOR_SQL_PAGE);
+        String[] sizes = params.get(QueryParamConstants.SQL_PARAMS_KEY_FOR_SQL_SIZE);
+        String[] sorts = params.get(QueryParamConstants.SQL_PARAMS_KEY_FOR_SQL_SORT);
+        Integer page = null;
+        Integer size = null;
+        Sort sort = null;
+        if (pages != null && pages.length > 0) {
+            page = Integer.valueOf(pages[0]);
+        }
+        if (sizes != null && sizes.length > 0) {
+            size = Integer.valueOf(sizes[0]);
+        }
+        if (sorts != null && sorts.length > 0) {
+            for (int i = 0; i < sorts.length; i++) {
+                String sortParam = sorts[i];
+                String[] sortArray = sortParam.split(",");
+                Sort sigleSort = null;
+                if (sortArray.length == 1) {
+                    sigleSort = new Sort(Sort.Direction.DESC, sorts[0]);
+                } else if (sortArray.length == 2) {
+                    String direction = sortArray[1];
+                    String fieldName = sortArray[0];
+                    if (QueryParamConstants.SQL_PARAMS_KEY_FOR_SQL_SORT_DESC.equalsIgnoreCase(direction)) {
+                        sigleSort = new Sort(Sort.Direction.DESC, fieldName);
+                    } else if (QueryParamConstants.SQL_PARAMS_KEY_FOR_SQL_SORT_ASC.equalsIgnoreCase(direction)) {
+                        sigleSort = new Sort(Sort.Direction.ASC, fieldName);
+                    }
+                }
+                if(sort == null) {
+                    sort = sigleSort;
+                }else {
+                    sort = sort.and(sigleSort);
+                }
+            }
+        }
+        if (page != null && size != null) {
+            return  PageRequest.of(page, size, sort);
+        }
+        return null;
+    }
+
+
+
+    public static String extractMoreClause(Map<String, String[]> params) {
+        String moreWhereClause = null;
+        String[] moreWhereClauses = params.get(QueryParamConstants.SQL_PARAMS_KEY_FOR_CUSTOMER_SQL_CONDITION_CLAUSE);
+        if (moreWhereClauses != null && moreWhereClauses.length > 0) {
+            moreWhereClause = " and " + moreWhereClauses[0];
+        }
+        return moreWhereClause;
+    }
+
+}
