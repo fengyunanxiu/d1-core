ab60959869018baabad021d269f6f13706217d7b
DAM <1414028747@qq.com>
2019-07-19 21:07:15 +0800

    commit

--- src/main/java/ai/sparklabinc/D1CoreApplication.java
+++ src/main/java/ai/sparklabinc/D1CoreApplication.java
@@ -2,12 +2,16 @@ package ai.sparklabinc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class D1CoreApplication {
 
     public static void main(String[] args) {
